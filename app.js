65fb63c9b2cf297335925cc79771c8c7aa6e5991
xainrizvi <74286334+xainrizvi@users.noreply.github.com>
2024-05-02 20:57:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,63 +1,3 @@
-// Load the AWS SDK
-var AWS = require('aws-sdk');
-var express = require('express');
-var bodyParser = require('body-parser');
-var multer = require('multer');
-
-// Set region for AWS SDKs
-AWS.config.region = process.env.REGION;
-
-// Create an S3 instance
-var s3 = new AWS.S3();
-
-var app = express();
-
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Configure multer for handling multipart/form-data
-var upload = multer({ dest: 'uploads/' });
-
-// Render form to upload image
-app.get('/', function(req, res) {
-    res.render('index', {
-        title: 'Upload Image'
-    });
-});
-
-// Handle image upload
-app.post('/upload', upload.single('image'), function(req, res) {
-    // File info
-    var file = req.file;
-    
-    // S3 parameters
-    var params = {
-        Bucket: process.env.S3_BUCKET_NAME,
-        Key: file.originalname,
-        Body: file.buffer,
-        ACL: 'public-read' // Make the uploaded file publicly accessible
-    };
-
-    // Upload file to S3
-    s3.upload(params, function(err, data) {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('Error uploading file to S3');
-        }
-        console.log('File uploaded successfully:', data.Location);
-        res.send('File uploaded successfully. <a href="' + data.Location + '">View Image</a>');
-    });
-});
-
-var port = process.env.PORT || 3000;
-
-var server = app.listen(port, function() {
-    console.log('Server running at http://127.0.0.1:' + port + '/');
-});
-
-/*
-	
 		
 // Load the AWS SDK
 var AWS = require('aws-sdk')
@@ -86,6 +26,3 @@ var port = process.env.PORT || 3000
 var server = app.listen(port, function () {
   console.log('Server running at http://127.0.0.1:' + port + '/')
 })
-
-
-*/
