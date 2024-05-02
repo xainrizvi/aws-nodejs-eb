01b3f2fd1b91afbea55907f8eee7636e44e8fa59
xainrizvi <74286334+xainrizvi@users.noreply.github.com>
2024-05-02 06:29:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Load the AWS SDK
-var AWS = require('aws-sdk')
+/*var AWS = require('aws-sdk')
 
 var express = require('express')
 var bodyParser = require('body-parser')
@@ -25,3 +25,54 @@ var port = process.env.PORT || 3000
 var server = app.listen(port, function () {
   console.log('Server running at http://127.0.0.1:' + port + '/')
 })
+/*
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+, routes = require('./routes')
+, link = require('./routes/link')
+, http = require('http')
+, config = require('./config').config
+, redis = require('redis')
+, utils = require('./utils')
+, path = require('path');
+
+var app = express();
+
+var cache = redis.createClient(config.redisPort, config.redisHost);
+
+function startTopLinksKeyScheduler(){
+	utils.scheduleAtMidnight(function(key){
+		cache.del(key);
+		startTopLinksKeyScheduler();
+	}, 60*60*1000, utils.todayKey());
+}
+
+startTopLinksKeyScheduler();
+
+app.configure(function(){
+	app.set('port', process.env.PORT || config.port);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+	app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.post('/link/add', link.add);
+app.get('/:id', link.redirect);
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log("Express server listening on port " + app.get('port'));
+});
